7b8221394e47a44c1552d1d5639e07cc69682f72
Quentin Gaultier <quentin.gaultier@gmail.com>
2017-06-12 15:54:31 +0200

    rendering jsx

--- src/client.js
+++ src/client.js
@@ -4,4 +4,4 @@ import ReactDOM from "react-dom";
 const app = document.getElementById('app');
 
 //ReactDOM.render(React.createElement('div', null, 'hello you'), app);
-ReactDOM.render(<p>Salut</p>, app);
+ReactDOM.render(<p>Salut!</p>, app);
--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,22 @@ var path = require('path');
 module.exports = {
     context: path.join(__dirname, "src"),
     entry: './client.js'
-    // , module: {
-    //     loaders: [
-    //         {
-    //             test: /\.jsx?$/,
-    //             exclude: /(node_modules)/,
-    //             loader: 'babel-loader'
-    //             // , query: {
-    //             //     presets: ['react', 'es2015', 'stage-0'],
-    //             //     plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
-    //             // }
-    //         }
-    //     ]
-    // }
+    , module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader'
+                , query: {
+                    presets: ['react']
+                }
+                // , query: {
+                //     presets: ['react', 'es2015', 'stage-0'],
+                //     plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+                // }
+            }
+        ]
+    }
     , output: {
         path: __dirname + "/bin/",
         filename: "client.bundle.js"
