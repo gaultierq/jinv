cf0def93b4bd050b60b0f590d9e0b3e6e4d8c589
Quentin Gaultier <quentin.gaultier@gmail.com>
2017-06-12 18:18:32 +0200

    basic layout structure

--- src/client.js
+++ src/client.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./components/Header"
+import Layout from "./pages/Layout"
 
 const app = document.getElementById('app');
 
-//ReactDOM.render(React.createElement('div', null, 'hello you'), app);
-ReactDOM.render(<p>Salut!</p>, app);
+const layout =
+    <Layout>
+        <div>This is body</div>
+    </Layout>;
+
+ReactDOM.render(layout, app);
