e9aacaa6d854002b85117611ca4a1e9dd87d8d31
Quentin Gaultier <quentin.gaultier@gmail.com>
2017-06-14 17:05:25 +0200

    inserting

--- server/index.js
+++ server/index.js
@@ -10,6 +10,12 @@ const config = require("../webpack.config");
 const express = require('express');
 const app = express();
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
 const compiler = require("webpack")(config);
 
 // --> getting rid of webpack-dev-server
